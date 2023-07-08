2c1174c3b45bb1776f0277328021c4594758ba82
Streamlet <streamlet@outlook.com>
2023-07-08 14:27:25 +0800

    use system crypto api on windows

--- src/utility/crypto.h
+++ src/utility/crypto.h
@@ -4,16 +4,24 @@
 namespace crypto {
 
 std::string MD5(const void *data, size_t length);
-std::string MD5(const std::filesystem::path &file_path);
+std::string MD5(const std::string &s);
+std::string MD5File(const std::filesystem::path &file_path);
 std::string SHA1(const void *data, size_t length);
-std::string SHA1(const std::filesystem::path &file_path);
+std::string SHA1(const std::string &s);
+std::string SHA1File(const std::filesystem::path &file_path);
+#ifndef _WIN32
 std::string SHA224(const void *data, size_t length);
-std::string SHA224(const std::filesystem::path &file_path);
+std::string SHA224(const std::string &s);
+std::string SHA224File(const std::filesystem::path &file_path);
+#endif
 std::string SHA256(const void *data, size_t length);
-std::string SHA256(const std::filesystem::path &file_path);
+std::string SHA256(const std::string &s);
+std::string SHA256File(const std::filesystem::path &file_path);
 std::string SHA384(const void *data, size_t length);
-std::string SHA384(const std::filesystem::path &file_path);
+std::string SHA384(const std::string &s);
+std::string SHA384File(const std::filesystem::path &file_path);
 std::string SHA512(const void *data, size_t length);
-std::string SHA512(const std::filesystem::path &file_path);
+std::string SHA512(const std::string &s);
+std::string SHA512File(const std::filesystem::path &file_path);
 
 } // namespace crypto

cbe983c4b2954172ef3a37fa206f074b1a3699b0
Streamlet <streamlet@outlook.com>
2023-07-09 13:01:52 +0800

    use boost test instead of google test

--- src/utility/url_test.cc
+++ src/utility/url_test.cc
@@ -1,178 +1,182 @@
 #include "url.h"
-#include <gtest/gtest.h>
+#include <boost/test/unit_test.hpp>
 
-TEST(url_test, normal) {
+BOOST_AUTO_TEST_SUITE(url_test)
+
+BOOST_AUTO_TEST_CASE(normal) {
   const char *url_string = "http://username:password@domain:80/path/to/page?query=value#fragment";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "username");
-  ASSERT_EQ(url.password, "password");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "80");
-  ASSERT_EQ(url.full_path, "/path/to/page?query=value#fragment");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "query=value");
-  ASSERT_EQ(url.fragment, "fragment");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "username");
+  BOOST_CHECK(url.password == "password");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "80");
+  BOOST_CHECK(url.full_path == "/path/to/page?query=value#fragment");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "query=value");
+  BOOST_CHECK(url.fragment == "fragment");
 }
 
-TEST(url_test, no_password) {
+BOOST_AUTO_TEST_CASE(no_password) {
   const char *url_string = "http://username@domain:80/path/to/page?query=value#fragment";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "username");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "80");
-  ASSERT_EQ(url.full_path, "/path/to/page?query=value#fragment");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "query=value");
-  ASSERT_EQ(url.fragment, "fragment");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "username");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "80");
+  BOOST_CHECK(url.full_path == "/path/to/page?query=value#fragment");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "query=value");
+  BOOST_CHECK(url.fragment == "fragment");
 }
 
-TEST(url_test, no_username_password) {
+BOOST_AUTO_TEST_CASE(no_username_password) {
   const char *url_string = "http://domain:80/path/to/page?query=value#fragment";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "80");
-  ASSERT_EQ(url.full_path, "/path/to/page?query=value#fragment");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "query=value");
-  ASSERT_EQ(url.fragment, "fragment");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "80");
+  BOOST_CHECK(url.full_path == "/path/to/page?query=value#fragment");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "query=value");
+  BOOST_CHECK(url.fragment == "fragment");
 }
 
-TEST(url_test, no_port) {
+BOOST_AUTO_TEST_CASE(no_port) {
   const char *url_string = "http://domain/path/to/page?query=value#fragment";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "/path/to/page?query=value#fragment");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "query=value");
-  ASSERT_EQ(url.fragment, "fragment");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "/path/to/page?query=value#fragment");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "query=value");
+  BOOST_CHECK(url.fragment == "fragment");
 }
 
-TEST(url_test, has_query_no_frament) {
+BOOST_AUTO_TEST_CASE(has_query_no_frament) {
   const char *url_string = "http://domain/path/to/page?query=value";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "/path/to/page?query=value");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "query=value");
-  ASSERT_EQ(url.fragment, "");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "/path/to/page?query=value");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "query=value");
+  BOOST_CHECK(url.fragment == "");
 }
 
-TEST(url_test, has_query_no_frament_has_sharp) {
+BOOST_AUTO_TEST_CASE(has_query_no_frament_has_sharp) {
   const char *url_string = "http://domain/path/to/page?query=value#";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "/path/to/page?query=value#");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "query=value");
-  ASSERT_EQ(url.fragment, "");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "/path/to/page?query=value#");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "query=value");
+  BOOST_CHECK(url.fragment == "");
 }
 
-TEST(url_test, no_query_has_frament) {
+BOOST_AUTO_TEST_CASE(no_query_has_frament) {
   const char *url_string = "http://domain/path/to/page#fragment";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "/path/to/page#fragment");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "");
-  ASSERT_EQ(url.fragment, "fragment");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "/path/to/page#fragment");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "");
+  BOOST_CHECK(url.fragment == "fragment");
 }
 
-TEST(url_test, no_query_has_question_mask_has_frament) {
+BOOST_AUTO_TEST_CASE(no_query_has_question_mask_has_frament) {
   const char *url_string = "http://domain/path/to/page?#fragment";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "/path/to/page?#fragment");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "");
-  ASSERT_EQ(url.fragment, "fragment");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "/path/to/page?#fragment");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "");
+  BOOST_CHECK(url.fragment == "fragment");
 }
 
-TEST(url_test, no_query_no_frament) {
+BOOST_AUTO_TEST_CASE(no_query_no_frament) {
   const char *url_string = "http://domain/path/to/page";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "/path/to/page");
-  ASSERT_EQ(url.path, "/path/to/page");
-  ASSERT_EQ(url.query, "");
-  ASSERT_EQ(url.fragment, "");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "/path/to/page");
+  BOOST_CHECK(url.path == "/path/to/page");
+  BOOST_CHECK(url.query == "");
+  BOOST_CHECK(url.fragment == "");
 }
 
-TEST(url_test, root_path) {
+BOOST_AUTO_TEST_CASE(root_path) {
   const char *url_string = "http://domain/";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "/");
-  ASSERT_EQ(url.path, "/");
-  ASSERT_EQ(url.query, "");
-  ASSERT_EQ(url.fragment, "");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "/");
+  BOOST_CHECK(url.path == "/");
+  BOOST_CHECK(url.query == "");
+  BOOST_CHECK(url.fragment == "");
 }
 
-TEST(url_test, no_path) {
+BOOST_AUTO_TEST_CASE(no_path) {
   const char *url_string = "http://domain";
-  Url url                = Url::Parse(url_string);
+  Url url = Url::Parse(url_string);
 
-  ASSERT_EQ(url.valid, true);
-  ASSERT_EQ(url.protocol, "http");
-  ASSERT_EQ(url.username, "");
-  ASSERT_EQ(url.password, "");
-  ASSERT_EQ(url.domain, "domain");
-  ASSERT_EQ(url.port, "");
-  ASSERT_EQ(url.full_path, "");
-  ASSERT_EQ(url.path, "");
-  ASSERT_EQ(url.query, "");
-  ASSERT_EQ(url.fragment, "");
+  BOOST_CHECK(url.valid == true);
+  BOOST_CHECK(url.protocol == "http");
+  BOOST_CHECK(url.username == "");
+  BOOST_CHECK(url.password == "");
+  BOOST_CHECK(url.domain == "domain");
+  BOOST_CHECK(url.port == "");
+  BOOST_CHECK(url.full_path == "");
+  BOOST_CHECK(url.path == "");
+  BOOST_CHECK(url.query == "");
+  BOOST_CHECK(url.fragment == "");
 }
+
+BOOST_AUTO_TEST_SUITE_END()

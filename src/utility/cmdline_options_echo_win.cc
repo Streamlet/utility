6b6751ee6a4ac934f0d1ceb25fe84d166db12cb3
Streamlet <streamlet@outlook.com>
2023-07-16 17:01:43 +0800

    use native string for command line

--- src/utility/cmdline_options_echo_win.cc
+++ src/utility/cmdline_options_echo_win.cc
@@ -1,21 +1,22 @@
 
 #include "cmdline_options.h"
+#include "native_string.h"
 #include <sstream>
 #include <stdio.h>
 #include <windows.h>
 
-int WINAPI wWinMain(__in HINSTANCE hInstance,
-                    __in_opt HINSTANCE hPrevInstance,
-                    __in LPTSTR lpCmdLine,
-                    __in int nShowCmd) {
+int WINAPI _tWinMain(__in HINSTANCE hInstance,
+                     __in_opt HINSTANCE hPrevInstance,
+                     __in LPTSTR lpCmdLine,
+                     __in int nShowCmd) {
   LPCTSTR cmdline = ::GetCommandLine();
-  std::wstringstream ss;
-  ss << L"Original: " << cmdline << L"\n";
+  native_string_stream ss;
+  ss << _T("Original: ") << cmdline << _T("\n");
   auto options = cmdline_options::parse(cmdline);
-  for (const auto &item : options) {
-    ss << item.first << L": " << item.second << L"\n";
+  for (const auto &item : options.parsed_map) {
+    ss << item.first << _T(": ") << item.second << _T("\n");
   }
-  std::wstring str = ss.str();
-  ::MessageBox(NULL, str.c_str(), L"Arguments", 0);
+  std::basic_string<TCHAR> str = ss.str();
+  ::MessageBox(NULL, str.c_str(), _T("Arguments"), 0);
   return 0;
 }
---
+++ src/utility/native_string.h
@@ -0,0 +1,86 @@
+#ifdef _WIN32
+
+#include <tchar.h>
+
+#ifdef _UNICODE
+
+#define native_string std::wstring
+#define native_string_view std::wstring_view
+#define native_string_stream std::wstringstream
+
+#else
+
+#define native_string std::string
+#define native_string_view std::string_view
+#define native_string_stream std::stringstream
+
+#endif
+
+#else
+
+#define _T(s) s
+#define TCHAR char
+
+#define native_string std::string
+#define native_string_view std::string_view
+#define native_string_stream std::stringstream
+
+// Locale-specific Information
+#define _tsetlocale setlocale
+
+// Input and Output
+#define _tfopen fopen
+#define _tfreopen freopen
+#define _tremove remove
+#define _trename rename
+
+#define _ftprintf fprintf
+#define _tprintf printf
+#define _stprintf sprintf
+#define _vftprintf vfprintf
+#define _vtprintf vprintf
+#define _vstprintf vsprintf
+#define _ftscanf fscanf
+#define _tscanf scanf
+#define _stscanf sscanf
+#define _fgetts fgets
+#define _fputts fputs
+#define _gettchar getchar
+#define _getts gets
+#define _puttchar putchar
+#define _putts puts
+#define _tperror perror
+
+// Utility functions
+#define _ttof atof
+#define _ttoi atoi
+#define _ttol atol
+#define _tcstod strtod
+#define _tcstol strtol
+#define _tcstoul strtoul
+#define _tsystem system
+#define _tgetenv getenv
+#define _tcscpy strcpy
+#define _tcsncpy strncpy
+#define _tcscat strcat
+#define _tcsncat strncat
+#define _tcscmp strcmp
+#define _tcsncmp strncmp
+#define _tcscoll strcoll
+#define _tcschr strchr
+#define _tcsrchr strrchr
+#define _tcsspn strspn
+#define _tcscspn strcspn
+#define _tcspbrk strpbrk
+#define _tcsstr strstr
+#define _tcslen strlen
+#define _tcserror strerror
+#define _tcstok strtok
+#define _tcsxfrm strxfrm
+
+// Time and Date functions
+#define _tasctime asctime
+#define _tctime ctime
+#define _tcsftime strftime
+
+#endif

72bd9f93c2c30466921e61a3ced585c635385a1b
Streamlet <streamlet@outlook.com>
2024-06-24 10:38:39 +0800

    format

--- src/process/cmdline_options_echo.cc
+++ src/process/cmdline_options_echo.cc
@@ -3,7 +3,6 @@
 #include "cmdline_options.h"
 #include <cstdio>
 
-
 int _tmain(int argc, const TCHAR *argv[]) {
   auto options = cmdline_options::parse(argc, argv);
   for (const auto &item : options.parsed_map) {

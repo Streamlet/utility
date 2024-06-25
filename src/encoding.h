5509bdb5f225fbc584fb8f7d411054efab67d9aa
Streamlet <streamlet@outlook.com>
2024-06-25 18:26:39 +0800

    fix

--- src/encoding.h
+++ src/encoding.h
@@ -6,18 +6,22 @@ namespace xl {
 
 namespace encoding {
 
-std::wstring utf8_to_utf16(const char *utf8, size_t length = -1);
+std::wstring utf8_to_utf16(const char *utf8, size_t length);
+std::wstring utf8_to_utf16(const char *utf8);
 std::wstring utf8_to_utf16(const std::string &utf8);
 
-std::string utf16_to_utf8(const wchar_t *utf16, size_t length = -1);
+std::string utf16_to_utf8(const wchar_t *utf16, size_t length);
+std::string utf16_to_utf8(const wchar_t *utf16);
 std::string utf16_to_utf8(const std::wstring &utf16);
 
 #ifdef _WIN32
 
-std::wstring ansi_to_utf16(const char *ansi, size_t length = -1);
+std::wstring ansi_to_utf16(const char *ansi, size_t length);
+std::wstring ansi_to_utf16(const char *ansi);
 std::wstring ansi_to_utf16(const std::string &ansi);
 
-std::string utf16_to_ansi(const wchar_t *ucs2, size_t length = -1);
+std::string utf16_to_ansi(const wchar_t *ucs2, size_t length);
+std::string utf16_to_ansi(const wchar_t *ucs2);
 std::string utf16_to_ansi(const std::wstring &ucs2);
 
 #endif

9ec5c94154be762f3bc232f56aa3559bf5059c28
Streamlet <streamlet@outlook.com>
2024-06-27 00:54:00 +0800

    fit scope_exit

---
+++ src/scope_exit.h
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   scope_exit.h
+//    Author:      Streamlet
+//    Create Time: 2016-09-30
+//    Description:
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include <functional>
+
+namespace xl {
+
+class scope_guard_impl_base {
+public:
+  scope_guard_impl_base() : dismissed(false) {
+  }
+  scope_guard_impl_base(scope_guard_impl_base &that) : dismissed(that.dismissed) {
+    that.dismiss();
+  }
+  ~scope_guard_impl_base() {
+  }
+
+protected:
+  template <typename J>
+  static void static_execute(J &j) {
+    if (!j.dismissed) {
+      j.execute();
+    }
+  }
+
+public:
+  void dismiss() const {
+    dismissed = true;
+  }
+
+private:
+  mutable bool dismissed;
+};
+
+typedef const scope_guard_impl_base &scope_guard;
+
+template <typename F>
+class scope_guard_impl : public scope_guard_impl_base {
+public:
+  scope_guard_impl(F fn) : scope_guard_impl_base(), m_fn(fn) {
+  }
+  ~scope_guard_impl() {
+    static_execute(*this);
+  }
+  void execute() {
+    m_fn();
+  }
+
+private:
+  F m_fn;
+};
+
+template <typename F>
+inline scope_guard_impl<F> make_guard(F f) {
+  return scope_guard_impl<F>(f);
+}
+} // namespace xl
+
+#define XL_BLOCK_EXIT_CONN_(s, t) s##t
+#define XL_BLOCK_EXIT_CONN(s, t) XL_BLOCK_EXIT_CONN_(s, t)
+#define XL_ON_BLOCK_EXIT(...)                                                                                          \
+  ::xl::scope_guard XL_BLOCK_EXIT_CONN(sg, __LINE__) = ::xl::make_guard(::std::bind(__VA_ARGS__))
---
+++ src/scope_exit_test.cc
@@ -0,0 +1,66 @@
+#include "scope_exit.h"
+#include <gtest/gtest.h>
+
+void function_set(int *variable, int value) {
+  *variable = value;
+}
+
+class functor_set {
+public:
+  void operator()(int *variable, int value) {
+    *variable = value;
+  }
+};
+
+class member_set {
+public:
+  void set(int *variable, int value) {
+    *variable = value;
+  }
+};
+
+auto lambda_set = [](int *variable, int value) {
+  *variable = value;
+};
+
+TEST(scope_exit_test, normal) {
+  int i = 0;
+  ASSERT_EQ(i, 0);
+  function_set(&i, 1);
+  ASSERT_EQ(i, 1);
+  functor_set fs;
+  fs(&i, 2);
+  ASSERT_EQ(i, 2);
+  member_set ms;
+  ms.set(&i, 3);
+  ASSERT_EQ(i, 3);
+  lambda_set(&i, 4);
+  ASSERT_EQ(i, 4);
+
+  i = 0;
+  ASSERT_EQ(i, 0);
+  {
+    i = -1;
+    ASSERT_EQ(i, -1);
+    XL_ON_BLOCK_EXIT(function_set, &i, 1);
+  }
+  ASSERT_EQ(i, 1);
+  {
+    i = -1;
+    ASSERT_EQ(i, -1);
+    XL_ON_BLOCK_EXIT(fs, &i, 2);
+  }
+  ASSERT_EQ(i, 2);
+  {
+    i = -1;
+    ASSERT_EQ(i, -1);
+    XL_ON_BLOCK_EXIT(&member_set::set, &ms, &i, 3);
+  }
+  ASSERT_EQ(i, 3);
+  {
+    i = -1;
+    ASSERT_EQ(i, -1);
+    XL_ON_BLOCK_EXIT(lambda_set, &i, 4);
+  }
+  ASSERT_EQ(i, 4);
+}

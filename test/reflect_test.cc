e539bb518453c22ef7b189f3340e45f51bdeda3c
Streamlet <streamlet@outlook.com>
2024-07-21 20:23:56 +0800

    add reflect

---
+++ test/reflect_test.cc
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <xl/reflect>
+
+namespace {
+
+XL_BEGIN_REFLECT(Foo)
+  XL_REFLECT_MEMBER(int, a)
+  XL_REFLECT_MEMBER(std::string, b)
+XL_END_REFLECT()
+
+} // namespace
+
+TEST(reflect_test, normal) {
+  ASSERT_EQ(strcmp(Foo::Field<0>::name(), "a"), 0);
+  ASSERT_EQ(strcmp(Foo::Field<1>::name(), "b"), 0);
+
+  Foo foo;
+  foo.a = 123;
+  foo.b = "abc";
+
+  ASSERT_EQ(Foo::Field<0>::value(foo), 123);
+  ASSERT_EQ(Foo::Field<1>::value(foo), "abc");
+
+  Foo::Field<0>::value(foo) = 456;
+  Foo::Field<1>::value(foo) = "def";
+
+  ASSERT_EQ(Foo::Field<0>::value(foo), 456);
+  ASSERT_EQ(Foo::Field<1>::value(foo), "def");
+}

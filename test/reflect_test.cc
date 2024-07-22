35ea21c92e0189876876e616e916a4e6fb78885b
Streamlet <streamlet@outlook.com>
2024-07-23 00:55:36 +0800

    optimize reflect

--- test/reflect_test.cc
+++ test/reflect_test.cc
@@ -17,8 +17,8 @@ XL_REFLECT_END()
 TEST(reflect_test, normal) {
   ASSERT_EQ(Foo::fields(), 2);
 
-  ASSERT_EQ(strcmp(Foo::Field<0>::type(), "int"), 0);
-  ASSERT_EQ(strcmp(Foo::Field<1>::type(), "const char *"), 0);
+  ASSERT_EQ(Foo::Field<0>::type(), typeid(int));
+  ASSERT_EQ(Foo::Field<1>::type(), typeid(const char *));
   ASSERT_EQ(strcmp(Foo::Field<0>::name(), "a"), 0);
   ASSERT_EQ(strcmp(Foo::Field<1>::name(), "b"), 0);
   ASSERT_EQ(Foo::Field<0>::offset(), 0);
@@ -26,8 +26,8 @@ TEST(reflect_test, normal) {
   ASSERT_EQ(Foo::Field<0>::size(), sizeof(int));
   ASSERT_EQ(Foo::Field<1>::size(), sizeof(const char *));
 
-  ASSERT_EQ(strcmp(Foo::field_type(0), "int"), 0);
-  ASSERT_EQ(strcmp(Foo::field_type(1), "const char *"), 0);
+  ASSERT_EQ(Foo::field_type(0), typeid(int));
+  ASSERT_EQ(Foo::field_type(1), typeid(const char *));
   ASSERT_EQ(strcmp(Foo::field_name(0), "a"), 0);
   ASSERT_EQ(strcmp(Foo::field_name(1), "b"), 0);
   ASSERT_EQ(Foo::field_offset(0), 0);

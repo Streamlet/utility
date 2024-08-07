918e220e9aae87917213fa8071a3c540375650e1
Streamlet <streamlet@outlook.com>
2024-08-07 20:20:51 +0800

    add enum and remove_all for fs

--- test/file_test.cc
+++ test/file_test.cc
@@ -2,6 +2,11 @@
 #include <xl/file>
 
 TEST(file_test, fs_operation) {
+  xl::fs::remove(_T("f"));
+  xl::fs::remove(_T("f1"));
+  xl::fs::remove_all(_T("d"));
+  xl::fs::remove_all(_T("d1"));
+
   ASSERT_EQ(xl::fs::exists(_T("f")), false);
   ASSERT_EQ(xl::fs::size(_T("f")), -1);
   ASSERT_EQ(xl::fs::touch(_T("f")), true);
@@ -34,7 +39,81 @@ TEST(file_test, fs_operation) {
   ASSERT_EQ(xl::fs::exists(_T("d1")), false);
 }
 
+TEST(file_test, fs_enum_remove_all) {
+  xl::fs::remove_all(_T("d1"));
+
+  ASSERT_EQ(xl::fs::mkdirs(_T("d1") XL_FS_SEP _T("d21") XL_FS_SEP _T("d31")), true);
+  ASSERT_EQ(xl::fs::mkdirs(_T("d1") XL_FS_SEP _T("d21") XL_FS_SEP _T("d32")), true);
+  ASSERT_EQ(xl::fs::touch(_T("d1") XL_FS_SEP _T("d21") XL_FS_SEP _T("f33")), true);
+  ASSERT_EQ(xl::fs::touch(_T("d1") XL_FS_SEP _T("d21") XL_FS_SEP _T("f34")), true);
+  ASSERT_EQ(xl::fs::mkdirs(_T("d1") XL_FS_SEP _T("d22") XL_FS_SEP _T("d31")), true);
+  ASSERT_EQ(xl::fs::mkdirs(_T("d1") XL_FS_SEP _T("d22") XL_FS_SEP _T("d32")), true);
+  ASSERT_EQ(xl::fs::touch(_T("d1") XL_FS_SEP _T("d22") XL_FS_SEP _T("f33")), true);
+  ASSERT_EQ(xl::fs::touch(_T("d1") XL_FS_SEP _T("d22") XL_FS_SEP _T("f34")), true);
+
+  std::vector<xl::native_string> dirs;
+  auto callback = [&dirs](const xl::native_string &path, bool is_dir) -> bool {
+    dirs.push_back(path);
+    return true;
+  };
+
+  xl::fs::enum_dir(_T("d1"), callback, false, false);
+  ASSERT_EQ(dirs, (std::vector<xl::native_string>{
+                      _T("d21"),
+                      _T("d22"),
+                  }));
+  dirs.clear();
+
+  xl::fs::enum_dir(_T("d1") XL_FS_SEP _T("d21"), callback, false, false);
+  ASSERT_EQ(dirs, (std::vector<xl::native_string>{
+                      _T("d31"),
+                      _T("d32"),
+                      _T("f33"),
+                      _T("f34"),
+                  }));
+  dirs.clear();
+
+  xl::fs::enum_dir(_T("d1") XL_FS_SEP _T("d21") XL_FS_SEP _T("d31"), callback, false, false);
+  ASSERT_EQ(dirs, (std::vector<xl::native_string>{}));
+  dirs.clear();
+
+  xl::fs::enum_dir(_T("d1"), callback, true, false);
+  ASSERT_EQ(dirs, (std::vector<xl::native_string>{
+                      _T("d21"),
+                      _T("d21") XL_FS_SEP _T("d31"),
+                      _T("d21") XL_FS_SEP _T("d32"),
+                      _T("d21") XL_FS_SEP _T("f33"),
+                      _T("d21") XL_FS_SEP _T("f34"),
+                      _T("d22"),
+                      _T("d22") XL_FS_SEP _T("d31"),
+                      _T("d22") XL_FS_SEP _T("d32"),
+                      _T("d22") XL_FS_SEP _T("f33"),
+                      _T("d22") XL_FS_SEP _T("f34"),
+                  }));
+  dirs.clear();
+
+  xl::fs::enum_dir(_T("d1"), callback, true, true);
+  ASSERT_EQ(dirs, (std::vector<xl::native_string>{
+                      _T("d21") XL_FS_SEP _T("d31"),
+                      _T("d21") XL_FS_SEP _T("d32"),
+                      _T("d21") XL_FS_SEP _T("f33"),
+                      _T("d21") XL_FS_SEP _T("f34"),
+                      _T("d21"),
+                      _T("d22") XL_FS_SEP _T("d31"),
+                      _T("d22") XL_FS_SEP _T("d32"),
+                      _T("d22") XL_FS_SEP _T("f33"),
+                      _T("d22") XL_FS_SEP _T("f34"),
+                      _T("d22"),
+                  }));
+  dirs.clear();
+
+  ASSERT_EQ(xl::fs::remove_all(_T("d1")), true);
+  ASSERT_EQ(xl::fs::exists(_T("d1")), false);
+}
+
 TEST(file_test, bin_and_utf8) {
+  xl::fs::remove(_T("f"));
+
   ASSERT_EQ(xl::file::write(_T("f"), "\xef\xbb\xbf"
                                      "你好"),
             true);
@@ -56,6 +135,7 @@ TEST(file_test, bin_and_utf8) {
 }
 
 TEST(file_test, utf16_le) {
+  xl::fs::remove(_T("f"));
 
   ASSERT_EQ(xl::file::write_text_utf16_le(_T("f"), L"你好"), true); // U+4F60 U+597D
   ASSERT_EQ(xl::file::read(_T("f")), "\xff\xfe\x60\x4f\x7d\x59");
@@ -67,6 +147,8 @@ TEST(file_test, utf16_le) {
 }
 
 TEST(file_test, utf16_be) {
+  xl::fs::remove(_T("f"));
+
   ASSERT_EQ(xl::file::write_text_utf16_be(_T("f"), L"你好"), true); // U+4F60 U+597D
   ASSERT_EQ(xl::file::read(_T("f")), "\xfe\xff\x4f\x60\x59\x7d");
   ASSERT_EQ(xl::file::read_text_utf8_bom(_T("f")), "");

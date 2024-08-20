ed5a477f41f00bfd037433c9071fe57621e2fac8
Streamlet <streamlet@outlook.com>
2024-08-20 15:43:04 +0800

    update

--- test/http_test.cc
+++ test/http_test.cc
@@ -1,6 +1,32 @@
 #include <gtest/gtest.h>
+#include <xl/file>
 #include <xl/http>
 #include <xl/json>
+#include <xl/process>
+
+class http_test : public ::testing::Test {
+protected:
+  static void SetUpTestCase() {
+    auto workdir = xl::path::dirname(xl::process::executable_path().c_str());
+    pid_ = xl::process::start(_T("python"), {_T("http_echo.py")}, workdir);
+    printf("Waiting for HTTP server ready...\n");
+    while (true) {
+      int status = xl::http::get("http://localhost:8080/", nullptr);
+      if (status == xl::http::StatusOK) {
+        break;
+      }
+      xl::process::sleep(100);
+    }
+  }
+
+  static void TearDownTestCase() {
+    xl::process::kill(pid_);
+  }
+
+  static int pid_;
+};
+
+int http_test::pid_ = 0;
 
 XL_JSON_BEGIN(HttpEchoResult)
   XL_JSON_MEMBER(std::string, version)
@@ -10,7 +36,7 @@ XL_JSON_BEGIN(HttpEchoResult)
   XL_JSON_MEMBER(std::unique_ptr<std::string>, body)
 XL_JSON_END()
 
-TEST(http_test, simple_get) {
+TEST_F(http_test, simple_get) {
   std::string response_body;
   int status = xl::http::get("http://localhost:8080/echo", xl::http::BufferWriter(&response_body));
   ASSERT_EQ(status, xl::http::StatusCode::StatusOK);
@@ -23,7 +49,7 @@ TEST(http_test, simple_get) {
   ASSERT_EQ(echo_result.body, nullptr);
 }
 
-TEST(http_test, get_with_header) {
+TEST_F(http_test, get_with_header) {
   xl::http::Headers request_header = {
       {"TestCase", "get_with_header"}
   };
@@ -44,7 +70,7 @@ TEST(http_test, get_with_header) {
   ASSERT_EQ(response_header.find("Content-Type")->second, "application/json");
 }
 
-TEST(http_test, simple_post) {
+TEST_F(http_test, simple_post) {
   std::string request_body = "request_body_content";
   std::string response_body;
   int status = xl::http::post("http://localhost:8080/echo", xl::http::BufferReader(request_body),
@@ -59,7 +85,7 @@ TEST(http_test, simple_post) {
   ASSERT_EQ(*echo_result.body, "request_body_content");
 }
 
-TEST(http_test, post_with_header) {
+TEST_F(http_test, post_with_header) {
   xl::http::Headers request_header = {
       {"TestCase", "post_with_header"}
   };
@@ -81,7 +107,7 @@ TEST(http_test, post_with_header) {
   ASSERT_EQ(response_header.find("Content-Type")->second, "application/json");
 }
 
-TEST(http_test, post_form) {
+TEST_F(http_test, post_form) {
   xl::http::FormData form_data = {
       {"key1",  "value1" },
       {"key2",  "value2" },
--- test/unittest.cc
+++ test/unittest.cc
@@ -1,23 +1,7 @@
 #include <gtest/gtest.h>
-#include <xl/file>
-#include <xl/http>
 #include <xl/native_string>
-#include <xl/process>
-#include <xl/scope_exit>
 
 int _tmain(int argc, TCHAR *argv[]) {
-  auto workdir = xl::path::dirname(xl::process::executable_path().c_str());
-  int pid = xl::process::start(_T("python"), {_T("http_echo.py")}, workdir);
-  XL_ON_BLOCK_EXIT(xl::process::kill, pid);
-  printf("Starting local HTTP server...\n");
-  while (true) {
-    int status = xl::http::get("http://localhost:8080/", nullptr);
-    if (status == xl::http::StatusOK) {
-      break;
-    }
-    xl::process::sleep(100);
-  }
-
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }

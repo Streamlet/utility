e7c134e0a8fe4bbb3cc29a0766d75db791c1c467
Streamlet <streamlet@outlook.com>
2024-08-22 14:47:10 +0800

    add file header

--- test/process_test.cc
+++ test/process_test.cc
@@ -1,20 +1,41 @@
+// MIT License
+//
+// Copyright (c) 2022 Streamlet (streamlet@outlook.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #include <cstdio>
 #include <xl/native_string>
 #include <xl/process>
 
 #if defined(_WIN32)
-#define SHELL_CMD _T("cmd")
+#define SHELL_CMD    _T("cmd")
 #define SHELL_SWITCH _T("/C")
 #define PING_CMD_10S _T("ping -n 10 127.0.0.1")
 #define PING_CMD_20S _T("ping -n 20 127.0.0.1")
 #elif defined(__APPLE__)
-#define SHELL_CMD _T("bash")
+#define SHELL_CMD    _T("bash")
 #define SHELL_SWITCH _T("-c")
 #define PING_CMD_10S _T("ping -t 10 127.0.0.1")
 #define PING_CMD_20S _T("ping -t 20 127.0.0.1")
 #else
-#define SHELL_CMD _T("sh")
+#define SHELL_CMD    _T("sh")
 #define SHELL_SWITCH _T("-c")
 #define PING_CMD_10S _T("ping -w 10 127.0.0.1")
 #define PING_CMD_20S _T("ping -w 20 127.0.0.1")
